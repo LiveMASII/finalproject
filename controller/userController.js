34ea3bf9f6e31959892a9bee346db9bed39255ec
LiveMASII <saenzmarcjr@gmail.com>
2025-04-26 02:01:12 -0700

    Added routes for create, getAllUsers, getUserById, and update

--- controller/userController.js
+++ controller/userController.js
@@ -16,3 +16,42 @@ export const create = async (req, res) => {
         res.status(500).json({ errorMessage: error.message });
     }
 };
+
+export const getAllUsers = async (req, res) => {
+    try {
+        const userData = await User.find();
+        if (!userData || userData.length === 0) {
+            return res.status(404).json({ errorMessage: 'No user data found' });
+        }
+        res.status(200).json(userData);
+    } catch (error) {
+        res.status(500).json({ errorMessage: error.message });
+    }
+}
+
+export const getUserById = async (req, res) => {
+    try {
+        const id  = req.params.id;
+        const userExists = await User.findById(id);
+        if (!userExists) {
+            return res.status(404).json({ errorMessage: 'User not found' });
+        }
+        res.status(200).json(userExists);
+    } catch (error) {
+        res.status(500).json({ errorMessage: error.message });
+    }
+}
+
+export const update = async (req, res) => {
+    try {
+        const id  = req.params.id;
+        const userExists = await User.findById(id);
+        if (!userExists) {
+            return res.status(404).json({ errorMessage: 'User not found' });
+        }
+        const updatedData = await User.findByIdAndUpdate(id, req.body, { new: true });
+        res.status(200).json({updatedData});
+    } catch (error) {
+        res.status(500).json({ errorMessage: error.message });
+    }
+}
